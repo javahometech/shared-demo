fc4efe6a342b6e2836309c714a966812352e9abd
javahometech <kammana.hari@gmail.com>
2020-11-11 07:50:47 +0530

    Update deployToTomcat.groovy

--- vars/deployToTomcat.groovy
+++ vars/deployToTomcat.groovy
@@ -1,3 +1,9 @@
 def call(ip){
+  sshagent(['tomcat-dev']) {
+    // copy war file to tomcat webapps location
+    sh "scp -o StrictHostKeyChecking=no target/myweb*.war ec2-user@${ip}:/opt/tomcat8/webapps/myweb.war"
+    sh "ssh ec2-user@${ip} /opt/tomcat8/bin/shutdown.sh"
+    sh "ssh ec2-user@${ip} /opt/tomcat8/bin/startup.sh"
+  }
   echo "Deploy to tomcat on ip ${ip}"
 }
